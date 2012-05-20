028f9c9587394df5e5bfb48cfb34d5693282a029
Mateusz Czaplinski <mateusz@czaplinski.pl>
2012-05-20 22:38:12 +0200

    some more refactorings, added limit for number of redirects in Yadis

--- yadis.go
+++ yadis.go
@@ -15,92 +15,107 @@ import (
 	"strings"
 )
 
-func Yadis(ID string) (io.Reader, error) {
-	return YadisVerbose(ID, nil)
+func Yadis(id string) (io.Reader, error) {
+	return YadisVerbose(id, nil)
 }
 
-func YadisVerbose(ID string, verbose *log.Logger) (io.Reader, error) {
-	r, err := YadisRequest(ID, "GET")
-	if err != nil || r == nil {
-		return nil, err
-	}
+func YadisVerbose(id string, verbose *log.Logger) (io.Reader, error) {
+	for i:=0; i<5; i++ {
+		r, err := YadisRequest(id)
+		if err != nil || r == nil {
+			return nil, err
+		}
 
+		body, redirect, err := YadisProcess(r, verbose)
+		if err != nil {
+			return body, err
+		}
+		if body != nil {
+            if verbose != nil {
+                verbose.Printf(`got xrds from "%s"`, id)
+            }
+			return body, nil
+		}
+		if redirect == "" {
+			return nil, nil
+		}
+		id = redirect
+	}
+    return nil, errors.New("Too many Yadis redirects")
+}
+
+func YadisProcess(r *http.Response, verbose *log.Logger) (body io.Reader, redirect string, err error) {
 	contentType := r.Header.Get("Content-Type")
 
 	// If it is an XRDS document, return the Reader
 	if strings.HasPrefix(contentType, "application/xrds+xml") {
-		if verbose != nil {
-			verbose.Printf("got xrds from \"%s\"", ID)
-		}
-		return r.Body, nil
+		return r.Body, "", nil
 	}
 
 	// If it is an HTML doc search for meta tags
 	if contentType == "text/html" {
 		url_, err := searchHTMLMetaXRDS(r.Body)
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
 		if verbose != nil {
-			verbose.Printf("fetching xrds found in html \"%s\"", url_)
+			verbose.Printf(`fetching xrds found in html "%s"`, url_)
 		}
-		return Yadis(url_)
+		return nil, url_, nil
 	}
 
 	// If the response contain an X-XRDS-Location header
 	xrds_location := r.Header.Get("X-Xrds-Location")
 	if len(xrds_location) > 0 {
 		if verbose != nil {
-			verbose.Printf("fetching xrds found in http header \"%s\"", xrds_location)
+			verbose.Printf(`fetching xrds found in http header "%s"`, xrds_location)
 		}
-		return Yadis(xrds_location)
+		return nil, xrds_location, nil
 	}
 
 	if verbose != nil {
 		verbose.Printf("Yadis fails out, nothing found. status=%#v", r.StatusCode)
 	}
 	// If nothing is found try to parse it as a XRDS doc
-	return nil, nil
+	return nil, "", nil
 }
 
-func YadisRequest(url_ string, method string) (resp *http.Response, err error) {
-	resp = nil
-
-	var request = new(http.Request)
-	var client = new(http.Client)
-	var Header = make(http.Header)
-
-	request.Method = method
-
-	request.URL, err = url.Parse(url_)
-	if err != nil {
-		return
+func YadisRequest(url_ string) (*http.Response, error) {
+	request := http.Request{
+		Method:        "GET",
+		Proto:         "HTTP/1.0",
+		ProtoMajor:    1,
+		ProtoMinor:    0,
+		ContentLength: 0,
+		Close:         true,
 	}
 
-	// Common parameters
-	request.Proto = "HTTP/1.0"
-	request.ProtoMajor = 1
-	request.ProtoMinor = 0
-	request.ContentLength = 0
-	request.Close = true
+	var err error
+	request.URL, err = url.Parse(url_)
+	if err != nil {
+		return nil, err
+	}
 
-	Header.Add("Accept", "application/xrds+xml")
-	request.Header = Header
+	header := http.Header{}
+	header.Add("Accept", "application/xrds+xml")
+	request.Header = header
 
 	// Follow a maximum of 5 redirections
+	client := http.Client{}
 	for i := 0; i < 5; i++ {
-		response, err := client.Do(request)
-
+		response, err := client.Do(&request)
 		if err != nil {
 			return nil, err
 		}
-		if response.StatusCode == 301 || response.StatusCode == 302 || response.StatusCode == 303 || response.StatusCode == 307 {
+
+		switch response.StatusCode {
+		case 301, 302, 303, 307:
 			location := response.Header.Get("Location")
 			request.URL, err = url.Parse(location)
 			if err != nil {
 				return nil, err
 			}
-		} else {
+		default:
 			return response, nil
 		}
 	}
